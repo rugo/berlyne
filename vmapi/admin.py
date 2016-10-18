9b6ecda5529f16c83dbd3c96a69cb6f92f05f742
Ruben Gonzalez <rg@ht11.org>
2016-10-18 11:56:59 +0200

    Removed djcelery from admin

--- vmapi/admin.py
+++ vmapi/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 
-from djcelery.models import TaskMeta
 from .models import *
 
 admin.site.register(VirtualMachine)
-admin.site.register(TaskMeta)
 admin.site.register(Task)
