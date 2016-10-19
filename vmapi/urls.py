bcb5fcfa6a4b6012c1ed33565061b7c2e840fc5d
Ruben Gonzalez <rg@ht11.org>
2016-10-19 12:04:14 +0200

    Changed reverse name

--- vmapi/urls.py
+++ vmapi/urls.py
@@ -5,23 +5,23 @@ urlpatterns = [
     url(r'^vm/(?P<vm_slug>[\w-]+)/',
         include(
             [
-                url(r'^status$', views.vm_status, name="vmapi:vm_status"),
-                url(r'^address$', views.vm_address, name="vmapi:vm_address"),
-                url(r'^tasks$', views.vm_tasks, name="vmapi:vm_tasks"),
-                url(r'^create/(?P<vagrant_name>[\w-]+)', views.vm_create, name="vmapi:vm_create"),
-                url(r'^start/(?P<provider>[\w-]+)', views.vm_start, name="vmapi:vm_start_prov"),
-                url(r'^start$', views.vm_start, name="vmapi:vm_start"),
-                url(r'^stop$', views.vm_stop, name="vmapi:vm_stop"),
-                url(r'^destroy$', views.vm_destroy, name="vmapi:vm_destroy"),
-                url(r'^destroy_db$', views.vm_destroy_db, name="vmapi:vm_destroy_db"),
-                url(r'^destroy_fs$', views.vm_destroy_fs, name="vmapi:vm_destroy_fs"),
+                url(r'^status$', views.vm_status, name="vmapi_vm_status"),
+                url(r'^address$', views.vm_address, name="vmapi_vm_address"),
+                url(r'^tasks$', views.vm_tasks, name="vmapi_vm_tasks"),
+                url(r'^create/(?P<vagrant_name>[\w-]+)', views.vm_create, name="vmapi_vm_create"),
+                url(r'^start/(?P<provider>[\w-]+)', views.vm_start, name="vmapi_vm_start_prov"),
+                url(r'^start$', views.vm_start, name="vmapi_vm_start"),
+                url(r'^stop$', views.vm_stop, name="vmapi_vm_stop"),
+                url(r'^destroy$', views.vm_destroy, name="vmapi_vm_destroy"),
+                url(r'^destroy_db$', views.vm_destroy_db, name="vmapi_vm_destroy_db"),
+                url(r'^destroy_fs$', views.vm_destroy_fs, name="vmapi_vm_destroy_fs"),
             ]
             ),
         ),
-    url(r'^task/(?P<task_id>[\w-]+)',
+    url(r'^task/(?P<task_id>[\w-]+)/',
         include(
             [
-                url(r'status$', views.task_state, name="vmapi:task_status"),
+                url(r'status$', views.task_state, name="vmapi_task_status"),
             ]
             ),
         ),
