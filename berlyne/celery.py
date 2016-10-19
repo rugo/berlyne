6420b3b57921ec53f0b74bfec3bbc184f0b78979
Ruben Gonzalez <rg@ht11.org>
2016-10-19 09:38:08 +0200

    Removed commented code

--- berlyne/celery.py
+++ berlyne/celery.py
@@ -1,25 +1,3 @@
-# from __future__ import absolute_import
-#
-# import os
-#
-# from celery import Celery
-#
-#
-# # set the default Django settings module for the 'celery' program.
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'berlyne.settings')
-# from django.conf import settings
-#
-# app = Celery('berlyne')
-#
-# # This should be changed to a string on windows
-# app.config_from_object('django.conf:settings')
-#
-# app.autodiscover_tasks(['vmapi'])
-#
-# @app.task(bind=True)
-# def fun(self):
-#     with open("/tmp/lol", "w") as f:
-#         f.write(b"dsdsdsd")
 from __future__ import absolute_import
 
 import os
