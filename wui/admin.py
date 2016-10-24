5baee1300ec3dd435e261b917edf4f08bdea457b
Ruben Gonzalez <rg@ht11.org>
2016-10-24 15:42:25 +0200

    Added support for protected flat pages

--- wui/admin.py
+++ wui/admin.py
@@ -1,13 +1,1 @@
 from django.contrib import admin
-from django.contrib.flatpages.admin import FlatPageAdmin
-from django.contrib.flatpages.models import FlatPage
-
-# Define a new FlatPageAdmin
-class FlatPageAdmin(FlatPageAdmin):
-    fieldsets = (
-        (None, {'fields': ('url', 'title', 'content', 'sites')}),
-    )
-
-# Re-register FlatPageAdmin
-admin.site.unregister(FlatPage)
-admin.site.register(FlatPage, FlatPageAdmin)
