2c6e2afa530ae5062134fb69f3a72fe41c25f8c3
Ruben Gonzalez <rg@ht11.org>
2016-11-23 15:24:58 +0100

    Fixed TODO, added correct exception handling

--- vmmanage/tasks.py
+++ vmmanage/tasks.py
@@ -2,6 +2,7 @@ from autotask.tasks import delayed_task
 from uptomate import Deployment
 from django.conf import settings
 from .models import State
+from subprocess import CalledProcessError
 
 MSG_SUCCESS = "Finished"
 
@@ -13,8 +14,7 @@ def run_on_vagr(vagr_depl, f, vm_db=None, callback=None, **kwargs):
         vm_db.state_set.add(State(name=vagr_depl.status().state), bulk=False)
         try:
             vm_db.ip_addr = vagr_depl.service_network_address()
-        # TODO: change to approp exception
-        except BaseException:
+        except CalledProcessError:
             # this does not work all the time, e.g. when the command
             # was 'stop', however, that should never affect the
             # result of the actual command called
